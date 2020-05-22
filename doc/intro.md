7f886b5974d7958cb004069d6b7bd50982c2f49d
Lei Wang <tendant@gmail.com>
2020-05-22 09:04:49 -0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-telemetry
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_telemetry/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-telemetry.core-test
+  (:require [clojure.test :refer :all]
+            [clj-telemetry.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
