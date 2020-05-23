09f3c99919b22686c9194cb27546a5e2671b39dc
Lei Wang <tendant@gmail.com>
2020-05-22 18:55:58 -0700

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-telemetry "0.1.0-SNAPSHOT"
+(defproject clj-telemetry "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
