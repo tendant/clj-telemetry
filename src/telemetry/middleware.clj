49854f7ca0b9f0d3df84fa233f014abed8a60ebd
Lei Wang <tendant@gmail.com>
2020-05-22 22:10:15 -0700

    enable to skip event

--- src/telemetry/middleware.clj
+++ src/telemetry/middleware.clj
@@ -2,17 +2,27 @@
   (:require [telemetry.tracing :as tracing]
             [clojure.string :as string]))
 
+(defn event-fn-skip-probe
+  [req]
+  (let [user-agent (get-in [:headers "user-agent"] req)]
+    (if (and user-agent
+             (string/includes? user-agent "probe"))
+      false
+      true)))
+
 (defn wrap-telemetry-tracing
   ([handler]
-   (wrap-telemetry-tracing handler {:default-event? true}))
+   (wrap-telemetry-tracing handler {:event-fn identity}))
   ([handler options]
    (fn [req]
      (let [span (tracing/create-span)
            method (:request-method req)
            uri (:uri req)
-           msg (format "%s %s: begin" (string/upper-case (name method)) uri)]
+           msg (format "%s %s: begin" (string/upper-case (name method)) uri)
+           event-fn (:event-fn options)]
        (try
-         (if (:default-event? options)
+         (if (and event-fn
+                  (event-fn req))
            (tracing/add-event span msg))
          (handler (assoc req :span span))
          (finally
