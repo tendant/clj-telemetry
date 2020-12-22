57b89ca3822d58b8a8979068a7d2ec1dc2890236
Lei Wang <tendant@gmail.com>
2020-12-21 21:57:25 -0800

    upgrade to openttelemetry 0.13.1

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [io.opentelemetry/opentelemetry-api "0.4.1"]
-                 [io.opentelemetry/opentelemetry-sdk "0.4.1"]
-                 [io.opentelemetry/opentelemetry-exporters-jaeger "0.4.1"]
-                 [io.grpc/grpc-protobuf "1.28.0"]
-                 [io.grpc/grpc-netty-shaded "1.28.0"]]
+                 [io.opentelemetry/opentelemetry-api "0.13.1"]
+                 [io.opentelemetry/opentelemetry-sdk "0.13.1"]
+                 [io.opentelemetry/opentelemetry-exporter-jaeger "0.13.1"]
+                 [io.grpc/grpc-protobuf "1.34.1"]
+                 [io.grpc/grpc-netty-shaded "1.34.1"]]
   :repl-options {:init-ns telemetry.tracing})
--- src/telemetry/tracing.clj
+++ src/telemetry/tracing.clj
@@ -1,173 +1,137 @@
 (ns telemetry.tracing
   (:require [clojure.walk :as walk])
-  (:import [io.opentelemetry OpenTelemetry]
+  (:import [io.opentelemetry.sdk.trace SdkTracerProvider]
+           [io.opentelemetry.api OpenTelemetry]
            [io.opentelemetry.sdk OpenTelemetrySdk]
-           [io.grpc ManagedChannel ManagedChannelBuilder]
-           [io.opentelemetry.exporters.jaeger JaegerGrpcSpanExporter]
-           [io.opentelemetry.sdk.trace.export SimpleSpansProcessor]
-           [io.opentelemetry.common AttributeValue]))
+           [io.opentelemetry.context.propagation ContextPropagators]
+           [io.opentelemetry.api.trace.propagation W3CTraceContextPropagator]
+           [io.opentelemetry.sdk.trace.export BatchSpanProcessor SimpleSpanProcessor]
+           [io.grpc ManagedChannelBuilder]
+           [io.opentelemetry.exporter.jaeger JaegerGrpcSpanExporter]
+           [io.opentelemetry.context Context]
+           [io.opentelemetry.api.common Attributes]))
 
-(defn- get-tracer
-  []
-  (-> (OpenTelemetry/getTracerProvider)
-      (.get "telemetry.tracing" "semver:1.0.0")))
-
-(defn- build-exporter-jaeger
+(defn build-exporter-jaeger
   [service-name ip port]
   (let [port (Integer. port)
         channel (-> (ManagedChannelBuilder/forAddress ip port)
                     (.usePlaintext)
                     (.build))
-        exporter (-> (JaegerGrpcSpanExporter/newBuilder)
+        exporter (-> (JaegerGrpcSpanExporter/builder)
                      (.setServiceName service-name)
                      (.setChannel channel)
                      (.setDeadlineMs 30000)
                      (.build))]
     exporter))
 
-(defn- build-spans-processor
+(defn build-batch-span-processor
   [exporter]
-  (-> (SimpleSpansProcessor/create exporter)))
+  (-> (BatchSpanProcessor/builder exporter)
+      .build))
 
-(defn create-spans-processor-jaeger
-  [service-name ip port]
-  (let [exporter (build-exporter-jaeger service-name ip port)]
-    (build-spans-processor exporter)))
+(defn build-simple-span-processor
+  [exporter]
+  (-> (SimpleSpanProcessor/builder exporter)
+      .build))
 
-(defn setup-span-processor
+(defn init-open-telemetry
   [span-processor]
-  (-> (OpenTelemetrySdk/getTracerProvider)
-      (.addSpanProcessor span-processor)))
+  (let [open-telemetry (.build (OpenTelemetrySdk/builder))]
+    (-> open-telemetry
+        .getTracerManagement
+        (.addSpanProcessor span-processor))
+    open-telemetry))
 
-(defn shutdown-provider
-  []
-  (-> (OpenTelemetrySdk/getTracerProvider)
+(defn shutdown-open-telemetry
+  [open-telemetry]
+  (-> (.getTraceManagement open-telemetry)
       (.shutdown)))
 
-(defn walk-kv
-  "Recursively transforms all map keys from keywords to strings and apply fv to value"
-  [m fv]
-  (let [f (fn [[k v]] (if (keyword? k) [(name k) (fv v)] [k (fv v)]))]
-    ;; only apply to maps
-    (walk/postwalk (fn [x] (if (map? x) (into {} (map f x)) x)) m)))
-
-(defn ->attr
-  [v]
-  (cond
-    (instance? java.lang.String v) (AttributeValue/stringAttributeValue v)))
-
-(defn ->attrs
-  [m]
-  (if m
-    (walk-kv m ->attr)))
-
-(defn get-current-span
-  []
-  (let [tracer (get-tracer)]
-    (.getCurrentSpan tracer)))
-
-(defn end-current-span
-  []
-  (-> (get-current-span)
-      (.end)))
-
-(defn set-current-span
-  [span]
-  (-> (get-tracer)
-      (.withSpan span)))
+(defn- get-tracer
+  [open-telemetry library-name]
+  (if open-telemetry
+    (.getTracer open-telemetry (or library-name "telemetry.tracing"))))
 
 (defn create-span
-  ([]
-   (create-span nil nil))
-  ([^java.lang.String id]
-   (create-span id nil))
-  ([^java.lang.String id parent-span]
-   (let [span-id (or id
-                     (str (java.util.UUID/randomUUID)))]
-     (if parent-span
-       (-> (get-tracer)
-           (.spanBuilder span-id)
-           (.setParent parent-span)
-           (.startSpan))
-       (-> (get-tracer)
-           (.spanBuilder span-id)
-           (.startSpan))))))
+  ([tracer id]
+   (create-span tracer id nil))
+  ([tracer id parent]
+   (let [span (if parent
+                (.setParent (.spanBuilder tracer id)
+                            (.with (Context/current) parent))
+                (.spanBuilder tracer id))]
+     (.startSpan span))))
 
-(defn end-span
-  [span]
+(defn span-attributes
+  [span attributes]
   (if span
-    (.end span)))
+    (doseq [[k v] attributes]
+      (.setAttribute span (str k) (str v)))))
+
+(defn attrs [m]
+  (let [builder (Attributes/builder)]
+    (doseq [[k v] m]
+      (.put builder (str k) (str v)))
+    (.build builder)))
 
 (defn add-event
   ([span message]
    (add-event span message nil))
   ([span message m]
    (if span
-     (if m
-       (.addEvent span message (->attrs m))
+     (if (and m
+              (not (empty? m)))
+       (.addEvent span message (attrs m))
        (.addEvent span message)))))
 
-(defn setup-test
-  []
-  ;; Make sure jaeger all in one server is started
-  (setup-span-processor (create-spans-processor-jaeger "example" "localhost" 14250))
+(comment
+  (def exporter (build-exporter-jaeger "test-service-name" "localhost" "14250"))
+  (def span-processor (build-simple-span-processor exporter))
+  (def open-telemetry (init-open-telemetry exporter))
+  (def tracer (get-tracer open-telemetry "test.tracing"))
   )
 
 (defn test-span
-  []
-  (let [span1 (create-span)]
+  [tracer]
+  (let [span1 (create-span tracer "span-1")]
     (add-event span1 "1. first event")
     (add-event span1 "1. second event")
-    (let [span-id (str (java.util.UUID/randomUUID))
-          span2 (create-span span-id)]
-      ;; (add-event span2 "span 2. first event")
+    (let [span2 (create-span tracer "span-2")]
+      (add-event span2 "span 2. first event")
       (add-event span2 "span 2. second event" {"attr1" "attr1 value"
                                                "attr2" "attr2 value2"})
-      (println "span2:" span2)
       (.end span2))
     (.end span1)))
 
 (defn test-parent-span
-  []
-  (let [root (create-span)]
-    (add-event root "root span")
-    (let [child (create-span nil root)]
+  [tracer]
+  (let [root (create-span tracer "test-parent-span")]
+    (add-event root "root event")
+    (let [child (create-span tracer "test-parent-span child" root)]
       (add-event child "child span")
       (add-event root "root span with child")
-      (println "current span in child" (get-current-span))
-      (add-event (get-current-span) "current span with child")
-      (end-span child)
-      (println "current span end child" (get-current-span))
+      (.end child)
       (add-event child "child span after end child"))
-    (println "current span in root" (get-current-span))
     (add-event root "root span after child")
-    (end-span root)
-    (add-event root "root span after end root")))
+    (add-event root "root span after end root")
+    (.end root)))
 
 (defn test-parallel-spans
-  [id]
-  (let [span (create-span id)]
-    (add-event span "begin")
-    (set-current-span span)
-    (add-event span "span: a: after set current span")
-    (add-event (get-current-span) "current: a: get-current-span")
+  [tracer]
+  (let [span-1 (create-span tracer "test-parallel-1")
+        span-2 (create-span tracer "test-parallel-2")]
+    (add-event span-1 "begin")
+    (add-event span-2 "event: a")
     (Thread/sleep 1000)
-    (add-event span "span: b: after set current span")
-    (add-event (get-current-span) "current: b: get-current-span")
+    (add-event span-1 "event: b")
     (Thread/sleep 1000)
-    (add-event span "span: c: after set current span")
-    (add-event (get-current-span) "current: c: get-current-span")
+    (add-event span-2 "event: c")
     (Thread/sleep 1000)
-    (add-event span "span: d:  after set current span")
-    (add-event (get-current-span) "current: d: get-current-span")
+    (add-event span-1 "event: d")
     (Thread/sleep 1000)
-    (add-event span "span: e: after set current span")
-    (add-event (get-current-span) "current: e: get-current-span")
+    (add-event span-2 "event: e")
     (Thread/sleep 1000)
-    (add-event span "span: after set current span")
-    (add-event (get-current-span) "current: get-current-span")
-    (end-span (get-current-span))
-    (add-event (get-current-span) "current: after end currrent")
-    (add-event span "span: after end current")
-    (end-span span)
-    (add-event span "span: after end span")))
+    (add-event span-1 "event: f")
+    (add-event span-2 "span: after end span")
+    (.end span-1)
+    (.end span-2)))
